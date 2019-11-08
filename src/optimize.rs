ec74db7e37cc7e5583f012947c3ac86612a1db73
David B. Dahl <dahl@stat.byu.edu>
2019-11-08 16:55:10 -0700

    Refractor code

--- src/optimize.rs
+++ src/optimize.rs
@@ -42,7 +42,18 @@ fn cmp_f64_with_enumeration(a: &(usize, f64), b: &(usize, f64)) -> Ordering {
     }
 }
 
-// Binder Loss
+pub trait Computer {
+    fn new_subset(&mut self, partition: &mut Partition);
+    fn speculative_add(&mut self, partition: &Partition, i: usize, subset_index: usize) -> f64;
+    fn add_with_index(&mut self, partition: &mut Partition, i: usize, subset_index: usize);
+    fn remove(&mut self, partition: &mut Partition, i: usize) -> usize;
+    fn expected_loss(&self) -> f64;
+    fn expected_loss_unnormalized(&self) -> f64;
+    fn final_loss_from_kernel(&self, kernel: f64) -> f64;
+}
+
+
+// Expectation of the Binder Loss
 
 #[derive(Debug)]
 struct BinderSubsetCalculations {
@@ -62,16 +73,18 @@ impl<'a> BinderComputer<'a> {
             psm,
         }
     }
+}
 
-    pub fn new_subset(&mut self, partition: &mut Partition) {
+impl<'a> Computer for BinderComputer<'a> {
+    fn new_subset(&mut self, partition: &mut Partition) {
         partition.new_subset();
         self.subsets.push(BinderSubsetCalculations {
             committed_loss: 0.0,
             speculative_loss: 0.0,
-        })
+        });
     }
 
-    pub fn speculative_add(&mut self, partition: &Partition, i: usize, subset_index: usize) -> f64 {
+    fn speculative_add(&mut self, partition: &Partition, i: usize, subset_index: usize) -> f64 {
         self.subsets[subset_index].speculative_loss = partition.subsets()[subset_index]
             .items()
             .iter()
@@ -81,12 +94,12 @@ impl<'a> BinderComputer<'a> {
         self.subsets[subset_index].speculative_loss
     }
 
-    pub fn add_with_index(&mut self, partition: &mut Partition, i: usize, subset_index: usize) {
+    fn add_with_index(&mut self, partition: &mut Partition, i: usize, subset_index: usize) {
         self.subsets[subset_index].committed_loss += self.subsets[subset_index].speculative_loss;
         partition.add_with_index(i, subset_index);
     }
 
-    pub fn remove(&mut self, partition: &mut Partition, i: usize) -> usize {
+    fn remove(&mut self, partition: &mut Partition, i: usize) -> usize {
         let subset_index = partition.label_of(i).unwrap();
         self.subsets[subset_index].committed_loss -= partition.subsets()[subset_index]
             .items()
@@ -102,167 +115,24 @@ impl<'a> BinderComputer<'a> {
         subset_index
     }
 
-    pub fn expected_loss(&self) -> f64 {
+    fn expected_loss(&self) -> f64 {
         2.0 * self.expected_loss_unnormalized() + self.psm.sum_of_triangle()
     }
 
-    pub fn expected_loss_unnormalized(&self) -> f64 {
+    fn expected_loss_unnormalized(&self) -> f64 {
         self.subsets
             .iter()
             .fold(0.0, |s, subset| s + subset.committed_loss)
     }
-}
 
-fn binder_ensure_empty_subset(
-    partition: &mut Partition,
-    binder: &mut BinderComputer,
-    max_label: usize,
-) {
-    match partition.subsets().last() {
-        None => binder.new_subset(partition),
-        Some(last) => {
-            if !last.is_empty() && partition.n_subsets() <= max_label {
-                binder.new_subset(partition)
-            }
-        }
+    fn final_loss_from_kernel(&self, kernel: f64) -> f64 {
+        2.0 * kernel + self.psm.sum_of_triangle()
     }
+
 }
 
-fn binder_micro_optimized_allocation<T: Rng>(
-    partition: &mut Partition,
-    binder: &mut BinderComputer,
-    i: usize,
-    probability_of_exploration: f64,
-    rng: &mut T
-) -> usize {
-    let max_label = partition.n_subsets() - 1;
-    let mut iter = (0..=max_label)
-        .map(|subset_index| binder.speculative_add(partition, i, subset_index))
-        .enumerate();
-    let take_best = if probability_of_exploration > 0.0 {
-        rng.gen_range(0.0, 1.0) >= probability_of_exploration
-    } else {
-        true
-    };
-    let subset_index = if take_best {
-        iter.min_by(cmp_f64_with_enumeration).unwrap().0
-    } else {
-        let mut first_best = iter.next().unwrap();
-        let second_best_option = iter.next();
-        if second_best_option.is_none() {
-            first_best.0
-        } else {
-            let mut second_best = second_best_option.unwrap();
-            if cmp_f64_with_enumeration(&first_best, &second_best) == Ordering::Greater {
-                std::mem::swap(&mut first_best, &mut second_best);
-            }
-            for tuple in iter {
-                if cmp_f64_with_enumeration(&tuple, &second_best) == Ordering::Less {
-                    if cmp_f64_with_enumeration(&tuple, &first_best) == Ordering::Less {
-                        second_best = first_best;
-                        first_best = tuple;
-                    } else {
-                        second_best = tuple;
-                    }
-                }
-            }
-            second_best.0
-        }
-    };
-    binder.add_with_index(partition, i, subset_index);
-    subset_index
-}
 
-pub fn minimize_binder_by_salso<T: Rng>(
-    max_label: usize,
-    psm: &SquareMatrixBorrower,
-    max_scans: u32,
-    n_permutations: u32,
-    probability_of_exploration: f64,
-    stop_time: std::time::SystemTime,
-    rng: &mut T,
-) -> (Vec<usize>, f64, u32, u32) {
-    let ni = psm.n_items();
-    let beta_distribution_option = if probability_of_exploration > 0.0 {
-        Some(Beta::new(1.0, 1.0/probability_of_exploration).unwrap())
-    } else { None };
-    let mut global_minimum = std::f64::INFINITY;
-    let mut global_best = Partition::new(ni);
-    let mut global_n_scans = 0;
-    let mut permutation: Vec<usize> = (0..ni).collect();
-    let mut permutations_counter = 0;
-    while permutations_counter < n_permutations {
-        let mut binder = BinderComputer::new(psm);
-        let mut partition = Partition::new(ni);
-        permutation.shuffle(rng);
-        // Initial allocation
-        let pr_explore = if max_scans == 0 {
-            0.0
-        } else {
-            match beta_distribution_option {
-                Some(beta) => beta.sample(rng),
-                None => -probability_of_exploration
-            }
-        };
-        for i in 0..ni {
-            binder_ensure_empty_subset(&mut partition, &mut binder, max_label);
-            let ii = unsafe { *permutation.get_unchecked(i) };
-            binder_micro_optimized_allocation(&mut partition, &mut binder, ii, pr_explore, rng);
-        }
-        // Sweetening scans
-        let mut stop_exploring = false;
-        let mut n_scans = 0;
-        while n_scans < max_scans {
-            if n_scans == max_scans - 1 {
-                stop_exploring = true;
-            }
-            permutation.shuffle(rng);
-            let mut no_change = true;
-            let pr_explore = if stop_exploring {
-                0.0
-            } else {
-                match beta_distribution_option {
-                    Some(beta) => beta.sample(rng),
-                    None => -probability_of_exploration
-                }
-            };
-            for i in 0..ni {
-                binder_ensure_empty_subset(&mut partition, &mut binder, max_label);
-                let ii = unsafe { *permutation.get_unchecked(i) };
-                let previous_subset_index = binder.remove(&mut partition, ii);
-                let subset_index = binder_micro_optimized_allocation(&mut partition, &mut binder, ii, pr_explore, rng);
-                if subset_index != previous_subset_index {
-                    no_change = false;
-                };
-            }
-            n_scans += 1;
-            if no_change {
-                if stop_exploring {
-                    break;
-                } else {
-                    stop_exploring = true;
-                }
-            }
-        }
-        let value = binder.expected_loss_unnormalized();
-        if value < global_minimum {
-            global_minimum = value;
-            global_best = partition;
-            global_n_scans = n_scans;
-        }
-        permutations_counter += 1;
-        if std::time::SystemTime::now() > stop_time {
-            break;
-        }
-    }
-    // Canonicalize the labels
-    global_best.canonicalize();
-    let labels = global_best.labels_via_copying();
-    let loss = 2.0 * global_minimum + psm.sum_of_triangle();
-    (labels, loss, global_n_scans, permutations_counter)
-}
-
-// Variation of Information Loss
+// Lower bound of the expectation of variation of information loss
 
 #[derive(Debug)]
 struct VarOfInforLBCacheUnit {
@@ -292,8 +162,10 @@ impl<'a> VarOfInfoLBComputer<'a> {
             psm,
         }
     }
+}
 
-    pub fn new_subset(&mut self, partition: &mut Partition) {
+impl<'a> Computer for VarOfInfoLBComputer<'a> {
+    fn new_subset(&mut self, partition: &mut Partition) {
         partition.new_subset();
         self.subsets.push(VarOfInfoLBSubsetCalculations {
             cached_units: Vec::new(),
@@ -302,7 +174,7 @@ impl<'a> VarOfInfoLBComputer<'a> {
         })
     }
 
-    pub fn speculative_add(&mut self, partition: &Partition, i: usize, subset_index: usize) -> f64 {
+    fn speculative_add(&mut self, partition: &Partition, i: usize, subset_index: usize) -> f64 {
         let subset_of_partition = &partition.subsets()[subset_index];
         if subset_of_partition.n_items() == 0 {
             self.subsets[subset_index]
@@ -345,7 +217,7 @@ impl<'a> VarOfInfoLBComputer<'a> {
         self.subsets[subset_index].speculative_loss - self.subsets[subset_index].committed_loss
     }
 
-    pub fn add_with_index(&mut self, partition: &mut Partition, i: usize, subset_index: usize) {
+    fn add_with_index(&mut self, partition: &mut Partition, i: usize, subset_index: usize) {
         for (index, subset) in self.subsets.iter_mut().enumerate() {
             if index == subset_index {
                 for cu in subset.cached_units.iter_mut() {
@@ -360,7 +232,7 @@ impl<'a> VarOfInfoLBComputer<'a> {
         partition.add_with_index(i, subset_index);
     }
 
-    pub fn remove(&mut self, partition: &mut Partition, i: usize) -> usize {
+    fn remove(&mut self, partition: &mut Partition, i: usize) -> usize {
         let subset_index = partition.label_of(i).unwrap();
         for cu in self.subsets[subset_index].cached_units.iter_mut() {
             cu.committed_sum -= unsafe { self.psm.get_unchecked((cu.item, i)) };
@@ -381,10 +253,10 @@ impl<'a> VarOfInfoLBComputer<'a> {
                     let nif = ni as f64;
                     nif * nif.log2()
                         - 2.0
-                            * self.subsets[subset_index]
-                                .cached_units
-                                .iter()
-                                .fold(0.0, |s, cu| s + cu.committed_contribution)
+                        * self.subsets[subset_index]
+                        .cached_units
+                        .iter()
+                        .fold(0.0, |s, cu| s + cu.committed_contribution)
                 }
             };
         partition.remove_and_relabel(i, |killed_subset_index, moved_subset_index| {
@@ -395,43 +267,50 @@ impl<'a> VarOfInfoLBComputer<'a> {
         subset_index
     }
 
-    pub fn expected_loss(&self) -> f64 {
+    fn expected_loss(&self) -> f64 {
         let nif = self.psm.n_items() as f64;
         (self.expected_loss_unnormalized() + vilb_expected_loss_constant(self.psm)) / nif
     }
 
-    pub fn expected_loss_unnormalized(&self) -> f64 {
+    fn expected_loss_unnormalized(&self) -> f64 {
         self.subsets
             .iter()
             .fold(0.0, |s, subset| s + subset.committed_loss)
     }
+
+    fn final_loss_from_kernel(&self, kernel: f64) -> f64 {
+        (kernel + vilb_expected_loss_constant(self.psm)) / (self.psm.n_items() as f64)
+    }
 }
 
-fn vilb_ensure_empty_subset(
+
+// General algorithm
+
+fn ensure_empty_subset<T: Computer>(
     partition: &mut Partition,
-    vilb: &mut VarOfInfoLBComputer,
+    computer: &mut T,
     max_label: usize,
 ) {
     match partition.subsets().last() {
-        None => vilb.new_subset(partition),
+        None => computer.new_subset(partition),
         Some(last) => {
             if !last.is_empty() && partition.n_subsets() <= max_label {
-                vilb.new_subset(partition)
+                computer.new_subset(partition)
             }
         }
     }
 }
 
-fn vilb_micro_optimized_allocation<T: Rng>(
+fn micro_optimized_allocation<T: Rng, U: Computer>(
     partition: &mut Partition,
-    vilb: &mut VarOfInfoLBComputer,
+    computer: &mut U,
     i: usize,
     probability_of_exploration: f64,
-    rng: &mut T,
+    rng: &mut T
 ) -> usize {
     let max_label = partition.n_subsets() - 1;
     let mut iter = (0..=max_label)
-        .map(|subset_index| vilb.speculative_add(partition, i, subset_index))
+        .map(|subset_index| computer.speculative_add(partition, i, subset_index))
         .enumerate();
     let take_best = if probability_of_exploration > 0.0 {
         rng.gen_range(0.0, 1.0) >= probability_of_exploration
@@ -463,18 +342,19 @@ fn vilb_micro_optimized_allocation<T: Rng>(
             second_best.0
         }
     };
-    vilb.add_with_index(partition, i, subset_index);
+    computer.add_with_index(partition, i, subset_index);
     subset_index
 }
 
-pub fn minimize_vilb_by_salso<T: Rng>(
+pub fn minimize_once_by_salso<'a, T: Rng, U: Computer>(
+    computer_factory: Box<dyn Fn(&'a SquareMatrixBorrower<'a>) -> U>,
     max_label: usize,
-    psm: &SquareMatrixBorrower,
+    psm: &'a SquareMatrixBorrower<'a>,
     max_scans: u32,
     n_permutations: u32,
     probability_of_exploration: f64,
     stop_time: std::time::SystemTime,
-    rng: &mut T
+    rng: &mut T,
 ) -> (Vec<usize>, f64, u32, u32) {
     let ni = psm.n_items();
     let beta_distribution_option = if probability_of_exploration > 0.0 {
@@ -486,7 +366,7 @@ pub fn minimize_vilb_by_salso<T: Rng>(
     let mut permutation: Vec<usize> = (0..ni).collect();
     let mut permutations_counter = 0;
     while permutations_counter < n_permutations {
-        let mut vilb = VarOfInfoLBComputer::new(psm);
+        let mut computer = computer_factory(psm);
         let mut partition = Partition::new(ni);
         permutation.shuffle(rng);
         // Initial allocation
@@ -499,9 +379,9 @@ pub fn minimize_vilb_by_salso<T: Rng>(
             }
         };
         for i in 0..ni {
-            vilb_ensure_empty_subset(&mut partition, &mut vilb, max_label);
+            ensure_empty_subset(&mut partition, &mut computer, max_label);
             let ii = unsafe { *permutation.get_unchecked(i) };
-            vilb_micro_optimized_allocation(&mut partition, &mut vilb, ii, pr_explore, rng);
+            micro_optimized_allocation(&mut partition, &mut computer, ii, pr_explore, rng);
         }
         // Sweetening scans
         let mut stop_exploring = false;
@@ -521,11 +401,10 @@ pub fn minimize_vilb_by_salso<T: Rng>(
                 }
             };
             for i in 0..ni {
-                vilb_ensure_empty_subset(&mut partition, &mut vilb, max_label);
+                ensure_empty_subset(&mut partition, &mut computer, max_label);
                 let ii = unsafe { *permutation.get_unchecked(i) };
-                let previous_subset_index = vilb.remove(&mut partition, ii);
-                let subset_index =
-                    vilb_micro_optimized_allocation(&mut partition, &mut vilb, ii, pr_explore, rng);
+                let previous_subset_index = computer.remove(&mut partition, ii);
+                let subset_index = micro_optimized_allocation(&mut partition, &mut computer, ii, pr_explore, rng);
                 if subset_index != previous_subset_index {
                     no_change = false;
                 };
@@ -539,7 +418,7 @@ pub fn minimize_vilb_by_salso<T: Rng>(
                 }
             }
         }
-        let value = vilb.expected_loss_unnormalized();
+        let value = computer.expected_loss_unnormalized();
         if value < global_minimum {
             global_minimum = value;
             global_best = partition;
@@ -553,12 +432,12 @@ pub fn minimize_vilb_by_salso<T: Rng>(
     // Canonicalize the labels
     global_best.canonicalize();
     let labels = global_best.labels_via_copying();
-    let loss = (global_minimum + vilb_expected_loss_constant(psm)) / (ni as f64);
+    let loss = computer_factory(psm).final_loss_from_kernel(global_minimum);
     (labels, loss, global_n_scans, permutations_counter)
 }
 
-pub fn minimize_by_salso<T: Rng>(
-    psm: &SquareMatrixBorrower,
+pub fn minimize_by_salso<'a, T: Rng>(
+    psm: &'a SquareMatrixBorrower,
     use_vilb: bool,
     max_size: usize,
     max_scans: u32,
@@ -577,7 +456,8 @@ pub fn minimize_by_salso<T: Rng>(
     let stop_time = std::time::SystemTime::now() + std::time::Duration::new(seconds, nanoseconds);
     if !parallel {
         if use_vilb {
-            minimize_vilb_by_salso(
+            minimize_once_by_salso(
+                Box::new(|psm: &'a SquareMatrixBorrower<'a>| VarOfInfoLBComputer::new(psm)),
                 max_label,
                 psm,
                 max_scans,
@@ -587,7 +467,8 @@ pub fn minimize_by_salso<T: Rng>(
                 rng
             )
         } else {
-            minimize_binder_by_salso(
+            minimize_once_by_salso(
+                Box::new(|psm: &'a SquareMatrixBorrower<'a>| BinderComputer::new(psm)),
                 max_label,
                 psm,
                 max_scans,
@@ -607,7 +488,8 @@ pub fn minimize_by_salso<T: Rng>(
                 let mut child_rng = IsaacRng::from_rng(&mut rng).unwrap();
                 s.spawn(move |_| {
                     let result = if use_vilb {
-                        minimize_vilb_by_salso(
+                        minimize_once_by_salso(
+                            Box::new(|psm: &'a SquareMatrixBorrower<'a>| VarOfInfoLBComputer::new(psm)),
                             max_label,
                             psm,
                             max_scans,
@@ -617,7 +499,8 @@ pub fn minimize_by_salso<T: Rng>(
                             &mut child_rng
                         )
                     } else {
-                        minimize_binder_by_salso(
+                        minimize_once_by_salso(
+                            Box::new(|psm: &'a SquareMatrixBorrower<'a>| BinderComputer::new(psm)),
                             max_label,
                             psm,
                             max_scans,
